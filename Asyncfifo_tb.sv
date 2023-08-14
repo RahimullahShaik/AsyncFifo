bc070b2b8a2b506c964fe169d12a9d8adf299027
Rahim Shaik <aashiq.sk444@gmail.com>
2023-08-13 23:19:45 -0500

    Wrote FIFO Full condition logic and test bench with some warnings
    Wrote FIFO Full condition logic and SV test bench with some warnings

---
+++ Asyncfifo_tb.sv
@@ -0,0 +1,69 @@
+//Test bench to test the FIFO design 
+module Asyncfifo_tb #(parameter data_Size = 8, address_Size = 5);
+        logic fifo_Full;  					//gets asserted when the FIFO is full
+		logic fifo_Empty; 					// gets asserted when the FIFO is empty
+		logic [data_Size-1:0]read_Data;  	//output data read from the FIFO by the receiver block
+		logic [data_Size-1:0]write_Data;  	//input data to be written to the FIFO by the transmitter block
+		logic w_Clk; 						    //Write clock, clock domain 1
+		logic r_Clk;  					    //Read clock, clock domain 2
+		logic w_Rst;       					//Write reset inorder to reset the write pointer 
+		logic r_Rst; 	   					    //Read reset inorder to reset the read pointer 
+		logic w_Inc;       					//To incremnet Write pointer 
+		logic r_Inc;  	   					// To increment Read pointer 
+
+	integer i;
+//Instantiating the DUT						
+Asyncfifo #(data_Size, address_Size) Asyncfifo (.fifo_Full(fifo_Full), .fifo_Empty(fifo_Empty), .read_Data(read_Data), .write_Data(write_Data)
+, .w_Clk(w_Clk), .r_Clk(r_Clk), .w_Rst(w_Rst), .r_Rst(r_Rst), .w_Inc(w_Inc), .r_Inc(r_Inc));
+
+// instantiating write and read clocks
+always #5 w_Clk = ~w_Clk;
+always #10 r_Clk = ~r_Clk;
+
+initial begin
+    write_Data = 0;
+    // w_Clk = 1'b0;
+    // r_Clk = 1'b0;
+    w_Inc = 1'b0;
+    r_Inc = 1'b0;
+end 
+
+initial begin
+    w_Rst = 1'b0;
+    repeat(5)@(posedge w_Clk);
+    w_Rst = 1'b1;
+    for(i=0; i<128; i++)begin 
+        if(i%2)
+            w_Inc = 1'b1;
+        else 
+            w_Inc = 1'b0;
+        @(posedge w_Clk);
+        if(w_Inc==1)begin 
+            if(fifo_Full!=1'b1) begin
+                write_Data = $urandom();
+                $display("Data %h is being written in the FIFO",write_Data);
+            end
+        end
+    end
+end 
+
+initial begin 
+    r_Rst = 1'b0;
+    repeat(5)@(posedge r_Clk);
+    r_Rst = 1'b1;
+    for(i=0; i<128; i++)begin 
+        if(i%2)
+            r_Inc = 1'b1;
+        else 
+            r_Inc = 1'b0;
+        @(posedge r_Clk);
+        if(r_Inc==1)begin 
+            if(fifo_Empty!=1'b1) begin
+                read_Data = $urandom();
+                $display("Data %h is being read from the FIFO",read_Data);
+            end
+        end
+    end 
+end 
+
+endmodule
---
+++ FIFO_Full.v
@@ -0,0 +1,44 @@
+//Verilog module for FIFO full logic 
+module FIFO_Full #(parameter address_Size = 5)(
+	output [address_Size-1:0]w_Addr,	//next Address to be written 
+	input 	w_Clk,				//write clock domain clock
+	input	w_Inc,				//asserted when the current address is written so that write address and pointer gets updated
+	output reg [address_Size:0]w_Ptr,	//Write pointer always points to the location to be written to
+	input   w_Rst,				//Write clock domain Reset
+	output reg fifo_Full,			//Assereted when FIFO is full
+	input   wsync_Rptr);			//Write clock synchronized Read pointer 
+
+	wire [address_Size:0]w_NextBin, w_NextGray;	//to store next address and pointer
+	reg [address_Size:0] w_Bin;			//to store current address to be written
+	wire w_full;
+//Assigning next address and pointer of the write domain 
+always@(posedge w_Clk, negedge w_Rst)begin 
+	if(!w_Rst)begin 
+		w_Bin <= 0;
+		w_Ptr <= 0;
+	end 
+	else begin 
+		w_Bin <= w_NextBin;
+		w_Ptr <= w_NextGray;
+	end
+end 
+
+//Assigning current adddress to be written to in the memory
+assign w_Addr = w_Bin;
+
+///Calculating gray counter value for updating next pointer and updating next address 
+assign w_NextBin = w_Bin + ( w_Inc + ~fifo_Full );
+assign w_NextGray = ( w_NextBin >> 1) ^ w_NextBin;
+
+//Comparing the pointers to see if the fifo full condition is met
+assign w_full = (w_NextGray == wsync_Rptr);
+
+//Logic to assert fifo full condition 
+always@(posedge w_Clk, negedge w_Rst)begin 
+	if(!w_Rst) 
+		fifo_Full <= 1'b1;
+	else 
+		fifo_Full <= w_full;
+end
+endmodule
+
